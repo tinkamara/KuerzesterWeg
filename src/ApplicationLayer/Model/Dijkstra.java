038ca9024a11cd5122eb9efaf0431f9c5f764503
tinkamara <85729793+tinkamara@users.noreply.github.com>
2021-06-21 13:49:19 +0200

    Static Model and DB-Access finished

---
+++ src/ApplicationLayer/Model/Dijkstra.java
@@ -0,0 +1,84 @@
+package ApplicationLayer.Model;
+
+import java.util.*;
+
+import static java.lang.Integer.MAX_VALUE;
+
+public class Dijkstra {
+    private final ArrayList<Node> emptyNodes;
+    private Node start;
+    private Node destination;
+    private int totalDistance = 0;
+
+
+    public Dijkstra(String start, String destination){
+    this.start = Node.getNode(start);
+    this.destination = Node.getNode(destination);
+    this.emptyNodes = Node.getExistingNodes();
+    this.initialize();
+
+    while (this.emptyNodes.size() > 0) {
+       Node nextNode = this.getNextNode(this.start);
+       if(nextNode.equals(this.destination)){
+           break;
+        }
+       this.start = nextNode;
+
+        }
+    }
+
+
+
+    public void initialize(){
+        for( Node node : this.emptyNodes) {
+            node.resetNode();
+        }
+        this.start.setDistance(0);
+        this.start.setPredecessor(null);
+        emptyNodes.remove(this.start);
+    }
+
+
+    public Node getNextNode(Node start){
+        int minimalDistance = MAX_VALUE;
+        Node closest = null;
+
+        TreeMap<Node, Integer> currentNeighbors = start.getNeighbors();
+
+        Collection<Map.Entry<Node, Integer>> c = currentNeighbors.entrySet();
+
+        for (Map.Entry<Node, Integer> neighbor : c) {
+            if (emptyNodes.contains(neighbor.getKey())) {
+                neighbor.getKey().setPredecessor(start);
+                neighbor.getKey().setDistance(neighbor.getValue());
+                if (neighbor.getValue() < minimalDistance) {
+                    closest = neighbor.getKey();
+                    minimalDistance = neighbor.getValue();
+                }
+                emptyNodes.remove(neighbor.getKey());
+            } else if (neighbor.getValue() + this.totalDistance < neighbor.getKey().getDistance()) {
+                neighbor.getKey().setPredecessor(start);
+                neighbor.getKey().setDistance(neighbor.getValue());
+                if (neighbor.getValue() < minimalDistance) {
+                    closest = neighbor.getKey();
+                    minimalDistance = neighbor.getValue();
+                }
+            }
+        }
+        totalDistance = totalDistance + minimalDistance;
+        return closest;
+    }
+
+    public int getTotalDistance() {
+        return totalDistance;
+    }
+    public ArrayList<String> getPath(){
+        ArrayList<String> path = new ArrayList<>();
+        while(this.destination.getPredecessor() != null){
+            path.add(0, this.destination.getCity());
+            this.destination = this.destination.getPredecessor();
+        }
+        path.add(0, this.destination.getCity());
+        return path;
+    }
+}
--- src/ApplicationLayer/Model/Graph.java
+++ src/ApplicationLayer/Model/Graph.java
@@ -10,18 +10,32 @@ public class Graph {
     private static  ArrayList<Node> existingNodes;
 
     public static void init(JSONArray mapData){
+        existingEdges = new ArrayList<Edge>();
         for ( Object line : mapData) {
             JSONObject jLine = (JSONObject) line;
             Edge edge = new Edge((String) jLine.get("cityA"), (String) jLine.get("cityB"), (int) (long) jLine.get("distance"));
+            existingEdges.add(edge);
         }
-        existingEdges = Edge.getExistingEdges();
         existingNodes = Node.getExistingNodes();
-        for (Node node : existingNodes){
-            System.out.println(node.getCity());
+
+        for( Node node : existingNodes){
+            for (Edge edge : existingEdges){
+                if (edge.getCityA().equals(node)) {
+                    node.addNeighbor(edge.getCityB(), edge.getDistance());
+                } else if(edge.getCityB().equals(node)){
+                    node.addNeighbor(edge.getCityA(), edge.getDistance());
+                }
+            }
 
         }
     }
 
+    public static ArrayList<Edge> getExistingEdges() {
+        return existingEdges;
+    }
 
+    public static ArrayList<Node> getExistingNodes() {
+        return existingNodes;
+    }
 }
 
