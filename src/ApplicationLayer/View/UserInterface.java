07ce488d341852affdde0f282f508d51eb7d7290
Lara0815 <86253005+Lara0815@users.noreply.github.com>
2021-06-22 16:40:04 +0200

    Elemente der GUI eingefügt, ausführen funktioniert noch nicht

--- src/ApplicationLayer/View/UserInterface.java
+++ src/ApplicationLayer/View/UserInterface.java
@@ -1,6 +1,15 @@
 package ApplicationLayer.View;
 
 import ApplicationLayer.Controller.UserController;
+import javafx.scene.layout.VBox;
+
+//neue Imports
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
@@ -11,13 +20,40 @@ public class UserInterface {
     private String selectedDestination;
     private UserController controller;
 
-
+    //Elemente der GUI
+    Stage window;
+    Scene scene;
+    Button button;
+    ComboBox<String> comboBox;
 
     public UserInterface( ArrayList<String> cities, UserController controller) {
         this.controller = controller;
         this.cities = cities;
-        this.init();
+        this.init(Gui);
     }
+
+    public Gui{
+
+        window = new Stage();
+        window.setTitle("ComboBox Demo");
+        button = new Button("Submit");
+
+        comboBox = new ComboBox<String>();
+        comboBox.getItems().addAll(
+                "A",
+                "B",
+                "C"
+        );
+
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(20, 20, 20, 20));
+        layout.getChildren().addAll(button);
+
+        scene = new Scene(layout, 300, 250);
+        window.setScene(scene);
+        window.show();
+    }
+
     public UserInterface( String error){
         this.error = error;
         this.init();
@@ -32,6 +68,7 @@ public class UserInterface {
     public void updateDistance(int distance){
 
     }
+
     public void updatePath(ArrayList<String> path){
 
     }
